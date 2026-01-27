5c6a9770c2c9d3e7a871eb85f95a36baa9eac597
JOHAAAAAAAN <seba1sza@gmail.com>
2026-01-27 14:44:00 -0300

    Enero

--- data.js
+++ data.js
@@ -4,13 +4,31 @@ const dataPartidos = [
     // R = Resultado ('V' = Victoria, 'D' = Derrota)
     
     // Partidos de ejemplo de Enero
-    { mes: 1, jugador: "Isra", resultado: "V", goles: 5 },
-    { mes: 1, jugador: "Mois", resultado: "D", goles: 2 },
-    { mes: 1, jugador: "Seba", resultado: "V", goles: 3 },
-    
+    { mes: 1, jugador: "Isra", resultado: "V", goles: 0 },
+    { mes: 1, jugador: "Mois", resultado: "V", goles: 0 },
+    { mes: 1, jugador: "Seba", resultado: "V", goles: 0 },
+
+    { mes: 1, jugador: "Isra", resultado: "V", goles: 0 },
+    { mes: 1, jugador: "Mois", resultado: "D", goles: 0 },
+    { mes: 1, jugador: "Seba", resultado: "D", goles: 0 },
+
+    { mes: 1, jugador: "Isra", resultado: "V", goles: 0 },
+    { mes: 1, jugador: "Mois", resultado: "V", goles: 0 },
+    { mes: 1, jugador: "Seba", resultado: "V", goles: 0 },
+
+    { mes: 1, jugador: "Isra", resultado: "V", goles: 0 },
+    { mes: 1, jugador: "Mois", resultado: "V", goles: 0 },
+    { mes: 1, jugador: "Seba", resultado: "D", goles: 0 },
+
+    { mes: 1, jugador: "Isra", resultado: "V", goles: 0 },
+    { mes: 1, jugador: "Seba", resultado: "V", goles: 0 },
+
+    { mes: 1, jugador: "Isra", resultado: "V", goles: 0 },
+    { mes: 1, jugador: "Mois", resultado: "V", goles: 0 },
+    { mes: 1, jugador: "Seba", resultado: "D", goles: 0 },
     // Partidos de ejemplo de Febrero
-    { mes: 2, jugador: "Isra", resultado: "V", goles: 4 },
-    { mes: 2, jugador: "Mois", resultado: "V", goles: 6 },
+    //{ mes: 2, jugador: "Isra", resultado: "V", goles: 4 },
+    //{ mes: 2, jugador: "Mois", resultado: "V", goles: 6 },
 ];
 
 const jugadores = ["Isra", "Mois", "Seba"]; // Tus amigos
